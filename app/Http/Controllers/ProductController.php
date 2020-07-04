6fdfcdc0b09e9880977259861cb21a97357cb1c1
alaaragab34 <alaaragab34@gmail.com>
2020-07-05 00:05:31 +0200

    change identifier & instance char type

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -17,7 +17,7 @@ class ProductController extends Controller
         if(auth()->user()){
             //Get coupon from DB if found to apply on cart && get cart && cart count
             $shopingCart = ShoppingCart::where('identifier', auth()->user()->id.'_default')->first();
-            $data = $this->getCarts($shopingCart->coupon_id);
+            $data = $shopingCart ? $this->getCarts($shopingCart->coupon_id) : $this->getCarts();
         }else{
             // Get cart && cart count
             $data = $this->getCarts();
